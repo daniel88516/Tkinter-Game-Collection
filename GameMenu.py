034d3fb3dfedc6ac402dfc4ba6c44b7e1ab84305
jjjghu <chiuliyou@gmail.com>
2025-04-25 22:29:21 +0800

    破紀錄

--- GameMenu.py
+++ GameMenu.py
@@ -45,7 +45,7 @@ class GameMenu:
             "Zombie": "Z_main.py",
             "Monty Hall": "ThreeHall.py",
             "井字遊戲": "TicTacToe.py",
-            "踩地雷": "MineSweeper.py",
+            "踩地雷": "MineSweeper_SinglePlayer.py",
         }
 
         for name in self.games:
